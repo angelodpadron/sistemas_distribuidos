458d21d8fa810831d01d9588c07cafda6bb33dd1
Angelo Padron <padron891@gmail.com>
2023-04-14 18:46:11 -0300

    added opty files
    - the store update function is beign worked on
    - a client for testing the system is still under development as well

---
+++ opty/handler.erl
@@ -0,0 +1,34 @@
+-module(handler).
+
+-export([start/3]).
+
+start(Client, Validator, Store) ->
+  spawn_link(fun() -> init(Client, Validator, Store) end).
+
+init(Client, Validator, Store) ->
+  handler(Client, Validator, Store, [], []).
+
+handler(Client, Validator, Store, Reads, Writes) ->
+  receive
+    {read, Ref, N} ->
+      case lists:keysearch(N, 1, Writes) of
+        {value, {N, _, Value}} ->
+          Client ! {value, Ref, Value},
+          handler(Client, Validator, Store, Reads, Writes);
+        false ->
+          Entry = store:lookup(N, Store),
+          Entry ! {read, Ref, self()},
+          handler(Client, Validator, Store, Reads, Writes)
+      end;
+    {Ref, Entry, Value, Time} ->
+      Client ! {value, Ref, Value},
+      handler(Client, Validator, Store, [{Entry, Time} | Reads], Writes);
+    {write, N, Value} ->
+      Entry = store:lookup(N, Store),
+      Added = [{N, Entry, Value} | Writes],
+      handler(Client, Validator, Store, Reads, Added);
+    {commit, Ref} ->
+      Validator ! {validate, Ref, Reads, Writes, Client};
+    abort ->
+      ok
+  end.
---
+++ opty/server.erl
@@ -0,0 +1,31 @@
+-module(server).
+
+-export([start/1, open/1, stop/1]).
+
+start(N) ->
+  Server = spawn(fun() -> init(N) end),
+  register(server, Server).
+
+stop(Server) ->
+  Server ! stop.
+
+init(N) ->
+  Store = store:new(N),
+  Validator = validator:start(),
+  server(Validator, Store).
+
+server(Validator, Store) ->
+  receive
+    {open, Client} ->
+      Client ! {Validator, Store},
+      server(Validator, Store);
+    stop ->
+      store:stop(Store)
+  end.
+
+open(Server) ->
+  Server ! {open, self()},
+  receive
+    {transaction, Validator, Store} ->
+      handler:start(self(), Validator, Store)
+  end.
