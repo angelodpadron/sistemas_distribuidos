b7b96cf9aa9ce0d78c740916929620321de0705c
Replit user <>
2023-06-19 18:07:55 -0300

    toty: added minor changes

--- toty/multicast_b.erl
+++ toty/multicast_b.erl
@@ -1,32 +1,28 @@
-- module(multicast_b).
+-module(multicast_b).
 
-- export ([start/4]).
+-export([start/4]).
 
 start(_, Master, Nodes, Jitter) ->
   spawn(fun() -> init(Master, Nodes, Jitter) end).
 
 init(Master, Nodes, Jitter) ->
   server(Master, Nodes, Jitter).
-                   
-server(Master, Nodes, Jitter) ->
-  jitter(Jitter),
-  receive
-  {nodeJoined, NewNode} ->
-    server(Master, [NewNode | Nodes], Jitter);
-  
-  {send, Msg} ->
-    lists:foreach(fun(Node) -> Node ! {deliver, Msg} 
-                    end, [self() | Nodes]), 
-      server(Master, Nodes, Jitter);
 
-  {deliver, Msg} ->
-    Master ! {receive_message, Msg},
-    server(Master, Nodes, Jitter)
+server(Master, Nodes, Jitter) ->
+  receive
+    {nodeJoined, NewNode} ->
+      server(Master, [NewNode | Nodes], Jitter);
+    {send, Msg} ->
+      lists:foreach(fun(Node) -> Node ! {deliver, Msg} end, [self() | Nodes]),
+      jitter(Jitter),
+      server(Master, Nodes, Jitter);
+    {deliver, Msg} ->
+      Master ! {receive_message, Msg},
+      server(Master, Nodes, Jitter)
   end.
 
-
 jitter(0) ->
-    ok;
+  ok;
 jitter(Jitter) ->
-    timer:sleep(
-        random:uniform(Jitter)).
+  timer:sleep(
+    random:uniform(Jitter)).
